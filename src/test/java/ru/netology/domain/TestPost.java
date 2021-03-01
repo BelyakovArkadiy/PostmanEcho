f1d2b4deb0c64c9d6f8d2e38fccab275a26ecffe
BelyakovArkadiy <belyakovarkadiy@gmail.com>
2021-03-01 12:31:00 +0600

    Initial commit

---
+++ src/test/java/ru/netology/domain/TestPost.java
@@ -0,0 +1,43 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostManTest {
+    @Test
+    void shouldTestPost() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("User id") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("User id"))
+        ;
+    }
+
+    @Test
+    void shouldTestPost2() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8") // Если отправлять текст не на латинице, выставляем кодировку)
+                .body("Пользователь")
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Пользователь"))
+        ;
+    }
+}
